6e90271a8ae99e7df7297930130f13dd27199c0e
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2024-02-09 15:38:04 +0700

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Hieu Le on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
