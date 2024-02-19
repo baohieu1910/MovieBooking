af3e3e9dff59f5f4c97261d45eb238cf4185e665
Bảo Hiếu <l.b.hieu.vmp@gmail.com>
2024-02-19 22:31:59 +0700

    add select seat view and save by userdefaults

--- MovieBooking/ViewModels/BookingDetailViewModel.swift
+++ MovieBooking/ViewModels/BookingDetailViewModel.swift
@@ -10,7 +10,25 @@ import SwiftUI
 class BookingDetailViewModel: ObservableObject {
     @Published var details: [BookingDetail] = []
     
-    func checkConflict(bookingCode: String) -> Bool {
+    init() {
+        load()
+    }
+    
+    func addBookingList(bookings: [BookingDetail]) {
+        for booking in bookings {
+            self.details.append(booking)
+        }
+        
+        save()
+    }
+    
+    func addBooking(booking: BookingDetail) {
+        self.details.append(booking)
+        
+        save()
+    }
+    
+    func checkConflicts(bookingCode: String) -> Bool {
         for detail in details {
             if bookingCode == detail.getCode() {
                 return true
@@ -21,10 +39,16 @@ class BookingDetailViewModel: ObservableObject {
     }
     
     func save() {
-        
+        guard let data = try? JSONEncoder().encode(details) else {
+            return
+        }
+        UserDefaults.standard.set(data, forKey: "Details")
     }
     
     func load() {
-        
+        guard let data = UserDefaults.standard.object(forKey: "Details") as? Data else {
+            return
+        }
+        self.details = try! JSONDecoder().decode([BookingDetail].self, from: data)
     }
 }
--- MovieBooking/Views/BookingView.swift
+++ MovieBooking/Views/BookingView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct BookingView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @ObservedObject var viewModel = BookingDetailViewModel()
+    
     let theaterName: String
     let movieID: Int
     let date: Date
@@ -15,31 +19,107 @@ struct BookingView: View {
     
     let columns = Array(repeating: GridItem(), count: 9)
     
+    @State var bookingStatus = [Bool](repeating: false, count: 64)
+    
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            VStack {
-                LazyVGrid(columns: columns) {
-                    ForEach(0..<63) { num in
-                        Button {
-                            print(getCode(seatNum: num))
-                        } label: {
-                            RoundedRectangle(cornerRadius: 5)
-//                                .stroke([0, 8, 9, 17].contains(num) ? Color.clear : Color.gray, lineWidth: 1)
-                                .stroke(Color.gray)
-                                .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+        VStack {
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    LazyVGrid(columns: columns) {
+                        ForEach(0..<63) { num in
+                            if viewModel.checkConflicts(bookingCode: getCode(seatNum: num)) {
+                                RoundedRectangle(cornerRadius: 5)
+                                    .fill(.gray)
+                                    .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                            } else {
+                                Button {
+                                    print(getCode(seatNum: num))
+                                    bookingStatus[num].toggle()
+                                    
+                                } label: {
+                                    if bookingStatus[num] {
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .fill(.orange)
+                                            .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                                    } else {
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray)
+                                            .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                                    }
+                                }
+                            }
                         }
                     }
                 }
-                
+                                
+                HStack {
+                    HStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.gray)
+                            .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                        
+                        Text(": Available")
+                    }
+                    
+                    HStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.orange)
+                            .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                        
+                        Text(": Selected")
+                    }
+                    
+                    HStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.gray)
+                            .frame(width: UIScreen.screenWidth / 12, height: UIScreen.screenWidth / 12)
+                        
+                        Text(": Booked")
+                    }
+                }
             }
             
-            
+            if countNumBooking() > 0 {
+                Button {
+                    var bookingList = [BookingDetail]()
+                    
+                    for index in bookingStatus.indices {
+                        if bookingStatus[index] {
+                            let booking = BookingDetail(movieID: movieID, theaterName: theaterName, date: date, time: time, seatNum: index)
+                            bookingList.append(booking)
+                        }
+                    }
+                    
+                    viewModel.addBookingList(bookings: bookingList)
+                    
+                    dismiss()
+                } label: {
+                    Text("Buy Ticket")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.orange)
+                        .cornerRadius(10)
+                }
+            }
         }
         .padding(.horizontal)
+        .navigationTitle("Select Seats")
     }
     
     func getCode(seatNum: Int) -> String {
         return theaterName + String(movieID) + date.dayMonthYear() + time + String(seatNum)
+    }
+    
+    func countNumBooking() -> Int {
+        var count = 0
+        
+        for status in bookingStatus {
+            if status {
+                count += 1
+            }
+        }
+        
+        return count
     }
 }
 
